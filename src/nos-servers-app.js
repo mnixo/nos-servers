51caba4df95804ba7298ebb4f62e78b15fa2a24c
Miguel Nixo <mnixo@nuxeo.com>
2018-09-28 20:16:31 +0100

    add eslint

--- src/nos-servers-app.js
+++ src/nos-servers-app.js
@@ -2,9 +2,7 @@ import { LitElement, html } from '@polymer/lit-element';
 
 class NOSServersApp extends LitElement {
   static get properties() {
-    return {
-      
-    };
+    return {};
   }
 
   constructor() {
