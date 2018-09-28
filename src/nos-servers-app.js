019695e457803e27a3819abe2ebfc2ee82b7215f
Miguel Nixo <mnixo@nuxeo.com>
2018-09-28 19:30:38 +0100

    initial commit

---
+++ src/nos-servers-app.js
@@ -0,0 +1,20 @@
+import { LitElement, html } from '@polymer/lit-element';
+
+class NOSServersApp extends LitElement {
+  static get properties() {
+    return {
+      
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div>Nuxeo Online Services servers.</div>
+    `;
+  }
+}
+window.customElements.define('nos-servers-app', NOSServersApp);
