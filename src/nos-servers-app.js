4274433259048cd1dc890c1538c71aeb5ca408a8
Miguel Nixo <mnixo@nuxeo.com>
2018-09-28 22:41:19 +0100

    add app-layout

--- src/nos-servers-app.js
+++ src/nos-servers-app.js
@@ -1,4 +1,7 @@
 import { LitElement, html } from '@polymer/lit-element';
+import '@polymer/app-layout/app-header/app-header';
+import '@polymer/app-layout/app-toolbar/app-toolbar';
+import '@polymer/paper-styles/paper-styles';
 
 class NOSServersApp extends LitElement {
   static get properties() {
@@ -11,8 +14,21 @@ class NOSServersApp extends LitElement {
 
   render() {
     return html`
-      <div>Nuxeo Online Services servers.</div>
+      <style>
+        app-header {
+          font-family: var(--paper-font-common-base_-_font-family);
+          background-color: #0066ff;
+          color: #fff;
+        }
+        app-drawer {
+          z-index: 1;
+        }
+      </style>
+      <app-header reveals>
+        <app-toolbar>NOS Servers</app-toolbar>
+      </app-header>
     `;
   }
+
 }
 window.customElements.define('nos-servers-app', NOSServersApp);
