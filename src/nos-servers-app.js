d141782d4fc7b6b57ab48400d0494da86e0aab9e
Miguel Nixo <mnixo@nuxeo.com>
2018-10-06 23:08:01 +0100

    add config dialog

--- src/nos-servers-app.js
+++ src/nos-servers-app.js
@@ -1,15 +1,25 @@
 import { LitElement, html } from '@polymer/lit-element';
 import '@polymer/app-layout/app-header/app-header';
 import '@polymer/app-layout/app-toolbar/app-toolbar';
+import '@polymer/iron-icons/iron-icons';
+import '@polymer/paper-icon-button/paper-icon-button';
 import '@polymer/paper-styles/paper-styles';
+import './nos-servers-config-dialog';
 
 class NOSServersApp extends LitElement {
   static get properties() {
-    return {};
+    return {
+      _servers: { type: Array },
+    };
   }
 
   constructor() {
     super();
+    this._servers = JSON.parse(localStorage.getItem('servers'));
+    if (!this._servers) {
+      this._servers = [];
+      localStorage.setItem('servers', JSON.stringify(this._servers));
+    }
   }
 
   render() {
@@ -23,11 +33,35 @@ class NOSServersApp extends LitElement {
         app-drawer {
           z-index: 1;
         }
+        app-toolbar {
+          justify-content: space-between;
+        }
       </style>
       <app-header reveals>
-        <app-toolbar>NOS Servers</app-toolbar>
+        <app-toolbar>
+          NOS Servers
+          <paper-icon-button icon="settings" @tap="${this._openConfigDialog.bind(this)}"></paper-icon-button>
+        </app-toolbar>
       </app-header>
+      <nos-servers-config-dialog id="configDialog" @config-submitted="${this._onConfigSubmitted.bind(this)}">
+      </nos-servers-config-dialog>
+      ${this._renderServers(this._servers)}
     `;
+  }
+
+  _openConfigDialog() {
+    this.shadowRoot.getElementById('configDialog').open(this._servers);
+  }
+
+  _onConfigSubmitted(e) {
+    this._servers = e.detail.config;
+    localStorage.setItem('servers', JSON.stringify(this._servers));
+  }
+
+  _renderServers(servers) {
+    return servers.map(s => html`
+      <div>${JSON.stringify(s)}</div>
+    `);
   }
 
 }
---
+++ src/nos-servers-config-dialog.js
@@ -0,0 +1,87 @@
+import { LitElement, html } from '@polymer/lit-element';
+import '@polymer/iron-autogrow-textarea/iron-autogrow-textarea';
+import '@polymer/paper-button/paper-button';
+import '@polymer/paper-dialog/paper-dialog';
+
+class NOSServersConfigDialog extends LitElement {
+  static get properties() {
+    return {
+      isValid: { type: Boolean }
+    };
+  }
+
+  constructor() {
+    super();
+    this.isValid = false;
+  }
+
+  render() {
+    return html`
+      <style>
+        paper-dialog {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+        }
+        div.text-area-container {
+          padding: 0.5em;
+          margin: 0 20px;
+          background-color: #eee;
+          max-height: 100%;
+          overflow-y: scroll;
+        }
+        iron-autogrow-textarea {
+          width: calc(100% - 40px);
+          padding: 0;
+          font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+          border: none;         
+        }
+        div.buttons {
+          min-height: 40px;
+          padding: 0;
+          margin: 20px;
+        }
+      </style>
+      <paper-dialog id="dialog" with-backdrop>
+        <h3>Edit Configuration</h3>
+        <div class="text-area-container">
+          <iron-autogrow-textarea maxlength="999" id="textArea" @value-changed="${this._onValueChanged.bind(this)}">
+          </iron-autogrow-textarea>
+        </div>
+        <div class="buttons">
+          <paper-button dialog-confirm raised @tap="${this._onSubmitConfig.bind(this)}" .disabled="${!this.isValid}">
+            ok
+          </paper-button>
+          <paper-button dialog-dismiss raised>
+            cancel
+          </paper-button>
+        </div>
+      </paper-dialog>
+    `;
+  }
+
+  open(config) {
+    this.shadowRoot.getElementById('textArea').value = JSON.stringify(config, null, 2);
+    this.shadowRoot.getElementById('dialog').open();
+  }
+
+  _onValueChanged() {
+    this.shadowRoot.getElementById('dialog').center();
+    try {
+      JSON.parse(this.shadowRoot.getElementById('textArea').value);
+      this.isValid = true;
+    } catch(error) {
+      this.isValid = false;
+    }
+  }
+
+  _onSubmitConfig() {
+    this.dispatchEvent(new CustomEvent('config-submitted', {
+      detail: {
+        config: JSON.parse(this.shadowRoot.getElementById('textArea').value),
+      },
+    }));
+  }
+
+}
+window.customElements.define('nos-servers-config-dialog', NOSServersConfigDialog);
